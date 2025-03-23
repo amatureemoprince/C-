8c9f0d775b57d5c0eff8dd69077cf5eb52abe9fa
lj <2069057986@qq.com>
2025-03-23 19:58:55 +0800

    链表相关操作

--- CodeThinking/LinkList.c
+++ CodeThinking/LinkList.c
@@ -2,29 +2,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <c++/x86_64-w64-mingw32/bits/os_defines.h>
 
 typedef struct LinkNode {
-  int data;
-  struct LinkNode *next;
+    int data;
+    struct LinkNode *next;
 }LinkNode;
 
-//没有头结点的操作
-LinkNode *removeElements(LinkNode *head, int val) {
-    if (head == NULL) {
-        return NULL;
-    }
+typedef struct LinkList {
+    int size;
+    LinkNode *data;
+}LinkList;
 
+LinkNode *removeElements(LinkNode **head, int val) {
     LinkNode *temp;
 
-    while (head->data == val) {
-        temp = head;
-        head = head->next;
+    // 处理头结点
+    while (head != NULL && (*head)->data == val) {
+        temp = *head;
+        *head = temp->next;
         free(temp);
     }
 
-    LinkNode *p = head;
+    LinkNode *p = *head;
 
+    // 处理剩余节点
     while (p != NULL && p->next != NULL) {
+        if (p->next->data == val) {
+            temp = p->next;
+            p->next = temp->next;
+            free(temp);
+        } else {
+            p = p->next;
+        }
+    }
+
+    return *head;
+}
+
+//因为不用修改head故可以直接传入LinkNode *head
+LinkNode *removeElementsWithHeadNode(LinkNode *head, int val) {
+    LinkNode *temp;
+    LinkNode *p = head;
+    while (p->next != NULL) {
         if (p->next->data == val) {
             temp = p->next;
             p->next = temp->next;
@@ -33,24 +53,94 @@ LinkNode *removeElements(LinkNode *head, int val) {
             p = p->next;
         }
     }
-
     return head;
 }
 
-//带有头结点
-LinkNode *removeElementsWithHeadNode(LinkNode *head, int val) {
-    LinkNode *temp;
-    LinkNode *p = head;
-    while (p->next != NULL) {
-        if (p->next->data == val) {
-            temp = p->next;
-            p = temp->next;
-            free(temp);
-        }else {
-            p = p->next;
-        }
+//以下的函数都默认使用头结点
+
+//初始化带有头结点的List
+LinkList *createList() {
+    LinkList *list = (LinkList *)malloc(sizeof(LinkList));
+    list->size = 0;
+    LinkNode *head = (LinkNode *)malloc(sizeof(LinkNode));
+    head->next = NULL;
+    head->data = 0;
+    list->data = head;
+    return list;
+}
+
+//获取第index个元素值
+int get(LinkList *list, int index) {
+    if (index < 0 || index >= list->size) {
+        return -1;
     }
-    return head;
+    LinkNode *p = list->data;
+    for (int i = 0; i < index + 1; i++) {
+        p = p->next;
+    }
+    return p->data;
+}
+
+//头插法
+void insertAtHead(LinkList *list, int val) {
+    if (list == NULL) {
+        return ;
+    }
+    LinkNode *head = list->data;
+    LinkNode *node = (LinkNode *)malloc(sizeof(LinkNode));
+    node->data = val;
+    node->next = head->next;
+    head->next = node;
+    list->size++;
+}
+//尾插法
+void insertAtTail(LinkList *list, int val) {
+    if (list == NULL) {
+        return ;
+    }
+    LinkNode *p = list->data;
+    while (p->next != NULL) {
+        p = p->next;
+    }
+    LinkNode *node = (LinkNode *)malloc(sizeof(LinkNode));
+    node->data = val;
+    node->next = p->next;
+    p->next = node;
+    list->size++;
+}
+//插入index位置
+void insertAtIndex(LinkList *list, int index, int val) {
+    if (list == NULL) {
+        return ;
+    }
+    if (index < 0 || index > list->size) {
+        return ;
+    }
+    LinkNode *head = list->data;
+    LinkNode* node = (LinkNode *)malloc(sizeof(LinkNode));
+    node->data = val;
+    for (int i = 0; i < index; i++) {
+        head = head->next;
+    }
+    node->next = head->next;
+    head->next = node;
+    list->size++;
+}
+
+void deleteAtIndex(LinkList *list, int index) {
+    if (list == NULL) {
+        return;
+    }
+    if (index < 0 || index >= list->size) {
+        return;
+    }
+    LinkNode *head = list->data;
+    for(int i = 0; i < index; i++) {
+        head = head->next;
+    }
+    LinkNode *temp = head->next;
+    head->next = temp->next;
+    free(temp);
 }
 
 void printList(LinkNode *head) {
@@ -62,9 +152,58 @@ void printList(LinkNode *head) {
     printf("NULL\n");
 }
 
+void coutList(LinkList *list) {
+    LinkNode *head = list->data;
+    while (head->next != NULL) {
+        printf("%d -> ", head->next->data);
+        head = head->next;
+    }
+    printf("NULL\n");
+}
 
-
-int main(){
-
-  return 0;
+// int main() {
+//     LinkNode *head = (LinkNode *)malloc(sizeof(LinkNode));
+//     head->data = 1;
+//     head->next = (LinkNode *)malloc(sizeof(LinkNode));
+//     head->next->data = 2;
+//     head->next->next = (LinkNode *)malloc(sizeof(LinkNode));
+//     head->next->next->data = 3;
+//     head->next->next->next = NULL;
+//
+//     printList(head);
+//
+//     removeElements(&head, 1);
+//     printList(head);
+//
+//     // 释放链表内存
+//     LinkNode *current = head;
+//     while (current != NULL) {
+//         LinkNode *temp = current;
+//         current = current->next;
+//         free(temp);
+//     }
+//
+//     return 0;
+// }
+int main() {
+    LinkList * link_list = createList();
+    for (int i = 0; i < 10; i++) {
+        insertAtHead(link_list, i);
+    }
+    printf("the insert at head 0-9 elements:\n");
+    coutList(link_list);
+    for (int i = 0; i < 10; i++) {
+        insertAtTail(link_list, i);
+    }
+    printf("the insert at tail 0-9 elements:\n");
+    coutList(link_list);
+    printf("the insert at index:\n");
+    insertAtIndex(link_list, 1, 5);
+    coutList(link_list);
+    printf("get index is 5 element: %d\n", get(link_list, 7));
+    printf("delete the indes:\n");
+    deleteAtIndex(link_list, 4);
+    coutList(link_list);
+    free(link_list);
+    return 0;
 }
