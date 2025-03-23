b33840cf9f6e29af614d74573ac28150269406b2
lj <2069057986@qq.com>
2025-03-23 11:44:40 +0800

    单链表带与不带头结点删除相同元素的处理

--- CodeThinking/LinkList.c
+++ CodeThinking/LinkList.c
@@ -1,3 +1,69 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct LinkNode {
+  int data;
+  struct LinkNode *next;
+}LinkNode;
+
+//没有头结点的操作
+LinkNode *removeElements(LinkNode *head, int val) {
+    if (head == NULL) {
+        return NULL;
+    }
+
+    LinkNode *temp;
+
+    while (head->data == val) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+
+    LinkNode *p = head;
+
+    while (p != NULL && p->next != NULL) {
+        if (p->next->data == val) {
+            temp = p->next;
+            p->next = temp->next;
+            free(temp);
+        }else {
+            p = p->next;
+        }
+    }
+
+    return head;
+}
+
+//带有头结点
+LinkNode *removeElementsWithHeadNode(LinkNode *head, int val) {
+    LinkNode *temp;
+    LinkNode *p = head;
+    while (p->next != NULL) {
+        if (p->next->data == val) {
+            temp = p->next;
+            p = temp->next;
+            free(temp);
+        }else {
+            p = p->next;
+        }
+    }
+    return head;
+}
+
+void printList(LinkNode *head) {
+    LinkNode *current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+
+
 int main(){
 
   return 0;
