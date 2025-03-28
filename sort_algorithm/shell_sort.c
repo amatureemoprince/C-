7690a715a395bb8e98560c8b47d1c091787483f9
lj <2069057986@qq.com>
2025-03-28 11:05:41 +0800

    希尔排序

--- sort_algorithm/shell_sort.c
+++ sort_algorithm/shell_sort.c
@@ -1,3 +1,26 @@
-int main(){
-  return 0;
+#include "global_methods.c"
+// 希尔排序: 根据传入的d不同会有不同的时间复杂度
+void shell_sort(int *arr, int len, int d) {
+    // 需要分成不同的组进行排序
+    for (d; d >= 1; d = d / 2) {
+        // 每组的排序的插入排序
+        for (int i = d; i < len; i++) {
+            int temp = arr[i];
+            int j;
+            // 插入排序
+            for (j = i; j >= d && arr[j - d] > temp; j -= d) {
+                arr[j] = arr[j - d];
+            }
+            arr[j] = temp;
+        }
+    }
+}
+
+int main() {
+    int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    int d = 2;
+    shell_sort(arr, len, d);
+    coutArray(arr, len);
+    return 0'
 }
