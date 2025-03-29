4a908dbb1879d33d7f3ba3058ad766de2e4da7e3
lj <2069057986@qq.com>
2025-03-29 09:14:17 +0800

    快速排序

--- sort_algorithm/quick_sort.c
+++ sort_algorithm/quick_sort.c
@@ -1,38 +1,39 @@
 #include "global_methods.c"
 //处理low到height的元素：low为起始索引，height为结束索引
-int partition(int *arr, int low, int height) {
-    //将小于基准元素的值都移动到low指向的位置
-    int privot = arr[low];
-    while (low < height) {
-        while (low < height && arr[height] >= privot) {
-            height--;
+int partition(int *nums, int low, int high) {
+    int pivot = nums[low];
+    while (low < high) {
+        //移动hight指针
+        while (low < high && nums[high] >= pivot) {
+            high--;
         }
-        if (low < height)
-            arr[low] = arr[height];
-        while (low < height && arr[low] <= privot) {
+        //填坑
+        if (low < high) nums[low] = nums[high];
+        while (low < high && nums[low] <= pivot) {
             low++;
         }
-        if (low < height)
-            arr[height] = arr[low];
+        //填坑
+        if (low < high) nums[high] = nums[low];
     }
-    arr[low] = privot;
+    //基准数放到合适的位置
+    nums[low] = pivot;
     return low;
 }
 //快速排序
-void quick_sort(int *arr, int low, int height){
-    if (arr == NULL) {
-        return;
-    }
-    if (low < height) {
-        int par_index = partition(arr, low, height);
-        partition(arr, low, par_index - 1);
-        partition(arr, par_index + 1, height);
+void quick_sort(int *nums, int low, int high){
+    if (low < high) {
+        int index = partition(nums, low, high);
+        quick_sort(nums, low, index-1);
+        quick_sort(nums, index+1, high);
     }
 }
 
 int main(){
-    int arr[8] = {2, 4, 3, 6, 9, 0, 2, 8};
+    int arr[8] = {2, 5, 3, 6, 9, 0, 10, 8};
     quick_sort(arr, 0, 7);
     coutArray(arr, 8);
+    int brr[8] = {2, 5, 3, 6, 9, 0, 10, 8};
+    quick_sort_exchange(brr, 0, 7);
+    coutArray(brr, 8);
     return 0;
 }
